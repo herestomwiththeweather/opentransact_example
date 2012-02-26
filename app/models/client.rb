c236360fad21641d814c26e25e2d4408daa65df6
Tom Brown <herestomwiththeweather@gmail.com>
2012-02-25 19:50:16 -0600

    client has_many refresh,access tokens

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,7 @@
 class Client < ActiveRecord::Base
   attr_accessible :name, :website, :redirect_uri
+  has_many :access_tokens
+  has_many :refresh_tokens
   belongs_to :person
 
   before_validation :setup, :on => :create
